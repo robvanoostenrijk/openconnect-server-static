bd85debf132d1870b581d9ce6343015dc7057f4d
Rob van Oostenrijk <robvanoostenrijk@users.noreply.github.com>
2023-01-20 21:02:57 +0400

    GitHub Actions: Extract artifacts should extract only current platform

--- extract-artifacts.sh
+++ extract-artifacts.sh
@@ -7,23 +7,20 @@ echo "[i] Clean dist folder"
 rm -f -R ./dist
 mkdir -p ./dist
 
-for PLATFORM in linux/amd64 linux/arm64 linux/arm/v7
-do
-    CONTAINER=$(docker create --platform ${PLATFORM} "${IMAGE}:${VERSION}")
-    echo "[i] Created container ${CONTAINER:0:12}"
+CONTAINER=$(docker create --platform ${PLATFORM} "${IMAGE}:${VERSION}")
+echo "[i] Created container ${CONTAINER:0:12}"
 
-    echo "[i] Extract assets"
-    mkdir -p ./dist/bin ./dist/sbin
-    docker cp "${CONTAINER}:/usr/local/bin/occtl" ./dist/bin
-    docker cp "${CONTAINER}:/usr/local/bin/ocpasswd" ./dist/bin
-    docker cp "${CONTAINER}:/usr/local/bin/ocserv-fw" ./dist/bin
-    docker cp "${CONTAINER}:/usr/local/sbin/ocserv" ./dist/sbin
-    docker cp "${CONTAINER}:/usr/local/sbin/ocserv-worker" ./dist/sbin
+echo "[i] Extract assets"
+mkdir -p ./dist/bin ./dist/sbin
+docker cp "${CONTAINER}:/usr/local/bin/occtl" ./dist/bin
+docker cp "${CONTAINER}:/usr/local/bin/ocpasswd" ./dist/bin
+docker cp "${CONTAINER}:/usr/local/bin/ocserv-fw" ./dist/bin
+docker cp "${CONTAINER}:/usr/local/sbin/ocserv" ./dist/sbin
+docker cp "${CONTAINER}:/usr/local/sbin/ocserv-worker" ./dist/sbin
 
-    echo "[i] Create distribution archive"
-    XZ_OPT=-9 tar -C ./dist -Jcvf ./dist/openconnect-server-${PLATFORM//\//-}.tar.xz bin/occtl bin/ocpasswd bin/ocserv-fw sbin/ocserv sbin/ocserv-worker
+echo "[i] Create distribution archive"
+XZ_OPT=-9 tar -C ./dist -Jcvf ./dist/openconnect-server-${PLATFORM//\//-}.tar.xz bin/occtl bin/ocpasswd bin/ocserv-fw sbin/ocserv sbin/ocserv-worker
 
-    echo "[i] Removing container ${CONTAINER:0:12}"
-    docker rm $CONTAINER
-    rm -f -R ./dist/bin ./dist/sbin
-done
+echo "[i] Removing container ${CONTAINER:0:12}"
+docker rm $CONTAINER
+rm -f -R ./dist/bin ./dist/sbin
