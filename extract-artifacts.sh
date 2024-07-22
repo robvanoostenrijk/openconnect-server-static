914c1da37d54bdec7a1a404f3f1cbf2a7229ed75
Rob van Oostenrijk <robvanoostenrijk@users.noreply.github.com>
2024-07-22 14:25:10 +0400

    Remove legacy linux/arm/v6

--- extract-artifacts.sh
+++ extract-artifacts.sh
@@ -6,7 +6,7 @@ echo "[i] Clean dist folder"
 rm -f -R ./dist
 mkdir -p ./dist
 
-for PLATFORM in linux/amd64 linux/arm64 linux/arm/v7 linux/arm/v6
+for PLATFORM in linux/amd64 linux/arm64 linux/arm/v7
 do
     CONTAINER=$(docker create --platform ${PLATFORM} "${IMAGE}:latest")
     echo "[i] Created container ${CONTAINER:0:12}"
