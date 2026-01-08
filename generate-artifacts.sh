bc8603aa19aaf120dd2858fa2898c8031f776fce
marx peng <35778015+ixmu@users.noreply.github.com>
2026-01-08 21:24:14 +0800

    Update generate-artifacts.sh

--- generate-artifacts.sh
+++ generate-artifacts.sh
@@ -2,9 +2,9 @@
 docker build -t ocserv-static .
 CONTAINER=$(docker create ocserv-static)
 
-mkdir -p ./dist/bin ./dist/sbin
+mkdir -p ./dist/bin ./dist/sbin ./dist/libexec
 
-for FILE in "occtl" "ocpasswd" "ocserv-fw"
+for FILE in "occtl" "ocpasswd"
 do
 	docker cp "${CONTAINER}:/usr/local/bin/${FILE}" ./dist/bin
 done
@@ -22,5 +22,5 @@ done
 docker rm "${CONTAINER}"
 docker rmi ocserv-static
 
-XZ_OPT=-9 tar -C ./dist -Jcvf ./dist/ocserv-static.tar.xz bin sbin
-rm -f -R ./dist/bin ./dist/sbin
+XZ_OPT=-9 tar -C ./dist -Jcvf ./dist/ocserv-static.tar.xz bin sbin libexec
+rm -f -R ./dist/bin ./dist/sbin ./dist/libexec
