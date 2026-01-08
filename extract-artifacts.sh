9d162de9c458058783a70102183868d7c334b9ee
marx peng <35778015+ixmu@users.noreply.github.com>
2026-01-08 21:23:25 +0800

    Update extract-artifacts.sh

--- extract-artifacts.sh
+++ extract-artifacts.sh
@@ -12,7 +12,7 @@ do
     echo "[i] Created container ${CONTAINER:0:12}"
 
     echo "[i] Extract assets"
-    mkdir -p ./dist/bin ./dist/sbin
+    mkdir -p ./dist/bin ./dist/sbin ./dist/libexec
     docker cp "${CONTAINER}:/usr/local/bin/occtl" ./dist/bin
     docker cp "${CONTAINER}:/usr/local/bin/ocpasswd" ./dist/bin
     docker cp "${CONTAINER}:/usr/local/libexec/ocserv-fw" ./dist/libexec
@@ -20,9 +20,9 @@ do
     docker cp "${CONTAINER}:/usr/local/sbin/ocserv-worker" ./dist/sbin
 
     echo "[i] Create distribution archive"
-    XZ_OPT=-9 tar -C ./dist -Jcvf ./dist/openconnect-server-${PLATFORM//\//-}.tar.xz bin/occtl bin/ocpasswd bin/ocserv-fw sbin/ocserv sbin/ocserv-worker
+    XZ_OPT=-9 tar -C ./dist -Jcvf ./dist/openconnect-server-${PLATFORM//\//-}.tar.xz bin/occtl bin/ocpasswd libexec/ocserv-fw sbin/ocserv sbin/ocserv-worker
 
     echo "[i] Removing container ${CONTAINER:0:12}"
     docker rm $CONTAINER
-    rm -f -R ./dist/bin ./dist/sbin
+    rm -f -R ./dist/bin ./dist/sbin ./dist/libexec
 done
